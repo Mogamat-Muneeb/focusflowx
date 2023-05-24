819b90e3cd8a53b4ec222d180920ac85b625267e
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-05-24 16:29:00 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
   return (
     <BrowserRouter>
      <Routes>
-     <Route path="login" element={<Login/>}/>
+     {/* <Route path="login" element={<Login/>}/> */}
      <Route path="/" element={<Home/>}/>
-     <Route path="register" element={<Register/>}/>
+     {/* <Route path="register" element={<Register/>}/> */}
 </Routes>
 </BrowserRouter>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBH5XNhyMNbvfXmtF7c2_oLjTtJI8EM8LE",
-    authDomain: "focusflowx.firebaseapp.com",
-    projectId: "focusflowx",
-    storageBucket: "focusflowx.appspot.com",
-    messagingSenderId: "130506357244",
-    appId: "1:130506357244:web:be6fd36c5fe8b229f91a79",
-    measurementId: "G-R5H7VELLQD"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
